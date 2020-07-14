5e49d199ce103edc5fca0564b7e95f6fb5eee907
amy-yung <amy.yung@ibm.com>
2020-07-14 19:39:50 -0400

    temp main page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import image from './pngfuel.com.png';
+import { Button, Link } from 'carbon-components-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="main-header">
+          <Link className="topLinks">Sign In</Link>
+          <Link className="topLinks">Learn More</Link>
+      </nav>
+      <h1 className="main-title">Home Schooling</h1>
+      <p className="main-text">Learn how we keep your children engaged in 
+        learning and entertained during quarantine
+      </p>
+      <Button className="class-button" kind='primary'>Join Class</Button>
+      <img className="main-image" alt="empty" src={image} />
     </div>
   );
 }
