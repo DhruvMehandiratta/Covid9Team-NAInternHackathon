b8ef176530a6ca8165e2956e10cdb9f68749b0d1
amy-yung <amy.yung@ibm.com>
2020-07-15 15:25:59 -0400

    add calendar page

--- my-app/src/Components/Calendar/Calendar.js
+++ my-app/src/Components/Calendar/Calendar.js
@@ -1,36 +1,17 @@
 import React from 'react';
 import './Calendar.css';
 import { Container } from 'react-bootstrap';
-import classroom from '../../assets/liveClassroom.jpg';
-import playdates from '../../assets/playdate.png';
-import games from '../../assets/games.png';
-import vids from '../../assets/eduVids.png';
 
-class Learn extends React.Component { 
+class Calendar extends React.Component { 
 	render() {
 		return (
-			<Container className="learnContainer">
-            <div className="learn-page">
-                <div className="learn-text-container">
-                    <h1 className="learn-text">
-                        Explore new classes, games, online playdates and more!
+			<Container className="calendarContainer">
+            <div className="calendar-page">
+                <div className="calendar-text-container">
+                    <h1 className="calendar-text">
+                        Click to sign up to our live classrooms taught by certified teachers
                     </h1>
-					</div>
-					<div className="content-container">
-						<div className="content-container1">
-							<img className="classroom" alt="empty" src={classroom} />
-							<button className="classroomButton" href="/calendar">Live Classrooms</button>
-
-							<img className="playdates" alt="empty" src={playdates} />
-							<button className="playdatesButton">Online Playdates</button>
-						</div>
-						<div className="content-container2">
-							<img className="vids" alt="empty" src={vids} />
-							<button className="vidsButton">Educational Videos</button>
-
-							<img className="games" alt="empty" src={games} />
-							<button className="gamesButton">Games</button>
-						</div>
+                    <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%237986CB&amp;ctz=America%2FNew_York&amp;src=Y292aWQxOXRlYW1oYWNrQGdtYWlsLmNvbQ&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%230B8043&amp;mode=WEEK&amp;title=Live%20Classes&amp;showPrint=0&amp;showCalendars=0" style={{border:"solid 1px #777", position:"relative", left:"200px", top: "-180px", "z-index": "1"}}  width="800" height="600" frameborder="0" scrolling="no"></iframe>
 					</div>
 				</div>
         </Container>
@@ -39,4 +20,4 @@ class Learn extends React.Component {
 }
 
 
-export default Learn;
+export default Calendar;
