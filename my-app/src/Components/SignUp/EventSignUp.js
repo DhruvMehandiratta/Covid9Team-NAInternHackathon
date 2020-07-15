00d3b5970eec352f6a6504173ce86276a6baa064
Dhruv Mehandiratta <dhruvmehandiratta@Dhruvs-MacBook-Pro.local>
2020-07-15 17:05:48 -0300

    added form action in eventsignup form

--- my-app/src/Components/SignUp/EventSignUp.js
+++ my-app/src/Components/SignUp/EventSignUp.js
@@ -9,7 +9,7 @@ export default function signup() {
             <body  className="body"> 
             
             <div style={{height: "100%", position: "fixed", zIndex:"1", top: 100, left:180, width:"50%", marginTop: "100px"}}>
-                <form>
+                <form action="http://localhost:3000/addevent" method="POST">
                     <label style={{fontFamily: "Poppins", fontSize:"25px"}}>Date:</label> <br/><br/>
                     <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro",height:"50px", width:"300px", fontFamily: "Poppins"}} type="text" id="event_date"></input><br/><br/>
                     <label style={{fontFamily: "Poppins", fontSize:"25px"}}>Start Time:</label><br/><br/>
@@ -17,7 +17,7 @@ export default function signup() {
                     <label style={{fontFamily: "Poppins", fontSize:"25px"}}>End Time:</label><br/><br/>
                     <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro", fontFamily: "Poppins",height:"50px", width:"300px"}} type="text" id="event_end_time"></input><br/><br/>
                     <label style={{fontFamily: "Poppins", fontSize:"25px"}}>Subject Description:</label><br/><br/>
-                    <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro", fontFamily: "Poppins",height:"50px", width:"300px"}} type="password" id="event_desc"></input><br/><br/><br/>
+                    <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro", fontFamily: "Poppins",height:"50px", width:"300px"}} id="event_desc"></input><br/><br/><br/>
                     <button style={{fontFamily: "Poppins", fontSize:"15px", marginLeft: "50px", zIndex: "9999", transition: "all 0.2s ease-in 0s", cursor: "pointer", backgroundColor: "#b959ea",height:"50px", width:"180px", borderRadius: "12px"}} type="button" id="event_signup_button">Create Session</button>       
                 </form>
             </div>
