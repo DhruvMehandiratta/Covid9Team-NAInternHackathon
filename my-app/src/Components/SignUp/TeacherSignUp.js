6d3a86f3b69ff90a440a3e6a1324a631d02b77bf
Dhruv Mehandiratta <dhruvmehandiratta@Dhruvs-MacBook-Pro.local>
2020-07-15 15:46:45 -0300

    added event reg page

--- my-app/src/Components/SignUp/TeacherSignUp.js
+++ my-app/src/Components/SignUp/TeacherSignUp.js
@@ -19,14 +19,14 @@ export default function signup() {
             <div style={{height: "100%", width: "50%", position: "fixed", zIndex:"1", top: 100, right:0, width:"50%", marginLeft:"50px", marginTop: "100px"}}>
                 <form>
                     <label style={{fontFamily: "Poppins", fontSize:"25px"}}>Name:</label> <br/><br/>
-                    <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro",height:"50px", width:"300px", fontFamily: "Arial"}} type="text" id="teacher_name"></input><br/><br/>
+                    <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro",height:"50px", width:"300px", fontFamily: "Poppins"}} type="text" id="teacher_name"></input><br/><br/>
                     <label style={{fontFamily: "Poppins", fontSize:"25px"}}>Phone Number:</label><br/><br/>
-                    <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro", fontFamily: "Arial",height:"50px", width:"300px"}} type="text" id="teacher_no"></input><br/><br/>
+                    <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro", height:"50px", width:"300px", fontFamily: "Poppins"}} type="text" id="teacher_no"></input><br/><br/>
                     <label style={{fontFamily: "Poppins", fontSize:"25px"}}>Email:</label><br/><br/>
-                    <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro", fontFamily: "Arial",height:"50px", width:"300px"}} type="text" id="teacher_email"></input><br/><br/>
+                    <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro", height:"50px", width:"300px", fontFamily: "Poppins"}} type="text" id="teacher_email"></input><br/><br/>
                     <label style={{fontFamily: "Poppins", fontSize:"25px"}}>Password:</label><br/><br/>
-                    <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro", fontFamily: "Arial",height:"50px", width:"300px"}} type="password" id="teacher_password"></input><br/><br/><br/>
-                    <button style={{fontFamily: "Poppins", fontSize:"15px", marginLeft: "100px", zIndex: "9999", transition: "all 0.2s ease-in 0s", cursor: "pointer", left:"50%", backgroundColor: "#b959ea",height:"50px", width:"100px", borderRadius: "12px"}} type="button" id="signup_button">Sign Up</button>                    
+                    <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro", height:"50px", width:"300px", fontFamily: "Poppins"}} type="password" id="teacher_password"></input><br/><br/><br/>
+                    <button style={{fontFamily: "Poppins", fontSize:"15px", marginLeft: "100px", zIndex: "9999", transition: "all 0.2s ease-in 0s", cursor: "pointer", left:"50%", backgroundColor: "#b959ea",height:"50px", width:"100px", borderRadius: "12px"}} type="button" id="teacher_signup_button">Sign Up</button>                    
                 </form>
             </div>
         </body>
