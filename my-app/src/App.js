e27608946051e4652b50291081c21b50ea940af1
amy-yung <amy.yung@ibm.com>
2020-07-14 21:22:56 -0400

    fixed page dimensions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+// import Learn from './Learn';
 import image from './pngfuel.com.png';
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Button, Link } from 'carbon-components-react'
 
 function App() {
   return (
     <div className="App">
-      <nav className="main-header">
+      <div className="main-content">
+        <header className="main-header">
           <Link className="topLinks">Sign In</Link>
           <Link className="topLinks">Learn More</Link>
-      </nav>
-      <h1 className="main-title">Home Schooling</h1>
-      <p className="main-text">Learn how we keep your children engaged in 
+        </header>
+        <h1 className="main-title">Home Schooling</h1>
+        <p className="main-text">Learn how we keep your children engaged in
         learning and entertained during quarantine
       </p>
-      <Button className="class-button" kind='primary'>Join Class</Button>
-      <img className="main-image" alt="empty" src={image} />
+        <Button className="class-button" kind='primary'>Join Class</Button>
+        <img className="main-image" alt="empty" src={image} />
+      </div>
     </div>
   );
 }
