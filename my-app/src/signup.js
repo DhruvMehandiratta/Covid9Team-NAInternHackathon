8908df77914425ceb0193d9e2ea19bdc426e2218
Dhruv Mehandiratta <dhruvmehandiratta@Dhruvs-MacBook-Pro.local>
2020-07-14 23:42:04 -0300

    made password in asterisks

--- my-app/src/signup.js
+++ my-app/src/signup.js
@@ -13,7 +13,7 @@ export default function signup() {
             <label style={{fontFamily: "Arial", fontSize:"25px"}}>Email:</label><br/><br/>
             <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro", fontFamily: "Arial",height:"50px", width:"300px"}} type="text" id="email"></input><br/><br/>
             <label style={{fontFamily: "Arial", fontSize:"25px"}}>Password:</label><br/><br/>
-            <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro", fontFamily: "Arial",height:"50px", width:"300px"}} type="text" id="password"></input><br/><br/><br/>
+            <input style={{fontSize:"25px", borderRadius: "10px", backgroundColor: "Gainsboro", fontFamily: "Arial",height:"50px", width:"300px"}} type="password" id="password"></input><br/><br/><br/>
             <button style={{backgroundColor: "Pink",height:"50px", width:"100px", borderRadius: "12px"}} type="button" id="signup_button">Sign Up</button>
         </form>
         </body>
