9f3b0474b35db385d60184d9e6dd86fd43be3a0b
Yiyi <yiyi.zhou@ibm.com>
2020-07-15 17:37:10 -0700

    sign up and log in as teacher/student

--- server/server.js
+++ server/server.js
@@ -1,43 +1,63 @@
 const express = require('express');
 const app = express();
+const bcrypt = require("bcrypt");
 const PORT = 5000;
 const cookieParser = require("cookie-parser");
+const { registerTeacher, registerStudent, loginStudent, loginTeacher } = require('./db.js')
 
 app.use(cookieParser());
 app.use(express.static('./public'));
 
-app.get('/test',(req,res)=>{
-  const customers = [{id:1, first:"john", last:"doe"},{id:2, first:"mary", last:"lin"}];
-  res.json(customers);
+//register as teacher
+app.post('/register/teacher', express.json(), async (req, res) => {
+  const { email, name, phone } = req.body;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const info = { email: email, name: name, phone: phone, password: hashedPassword };
+  try {
+    await registerTeacher(info)
+    res.status(200).json({ message: "Teacher successfully registered" })
+  } catch (err) {
+    res.status(409).json({ message: err })
+  }
 })
-//check if logged in
-app.get("/session", (req, res) => {
-    const sid = req.cookies.sid;
-    if (!sid) {
-      res.status(401).json({ error: "LOGIN_REQUIRED" });
-      return;
-    }
-    if (!sessions[sid]) {
-      res.clearCookie("sid");
-      res.status(403).json({ error: "LOGIN_UNAUTHORIZED" });
-      return;
-    }
-    res.status(200).json({ username: username });
-  });
-
-//log in
-app.post("/session", express.json(), (req, res) => {
-  const { username,password } = req.body;
-  console.log(req.body);
-  res.clearCookie("sid");
-  if (!username) {
-    res.status(401).json({ error: "LOGIN_REQUIRED" });
-  } else {
-    const session = addSession({ username });
-    res.cookie("sid", session.id);
-    res.status(200).json({ username: session.username });
+//register as student
+app.post('/register/student', express.json(), async (req, res) => {
+  const { email, parent_name, child_name, phone } = req.body;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const info = { email: email, parent_name: parent_name, child_name: child_name, phone: phone, password: hashedPassword };
+  try {
+    await registerStudent(info)
+    res.status(200).json({ message: "Student successfully registered" })
+  } catch (err) {
+    res.status(409).json({ message: "This email address has already been registered" })
+  }
+})
+//log in as a teacher
+app.post("/login/teacher", express.json(), async (req, res) => {
+  const { email, name, phone } = req.body;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const info = { email: email, name: name, phone: phone, password: hashedPassword };
+  try {
+    await loginTeacher(info)
+    res.status(200).json({ message: "Teacher successfully logged in" });
+  } catch (err) {
+    res.status(409).json({ message: err })
   }
 });
+
+//log in as a student
+app.post("/login/student", express.json(), async (req, res) => {
+  const { email, parent_name, child_name, phone } = req.body;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const info = { email: email, parent_name: parent_name, child_name: child_name, phone: phone, password: hashedPassword };
+  try {
+    await loginStudent(info)
+    res.status(200).json({ message: "Student successfully logged in" });
+  } catch (err) {
+    res.status(409).json({ message: err })
+  }
+});
+
 //log out
 app.delete("/session", (req, res) => {
   const sid = req.cookies.sid;
@@ -45,6 +65,7 @@ app.delete("/session", (req, res) => {
   deleteSession(sid);
   res.sendStatus(200);
 });
-app.listen(PORT, ()=>{
+
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
